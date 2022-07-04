29fb70d2586af9d19ea01c2eaaa26ea1cbdf61f8
mark_houlli <markhoulli@protonmail.com>
2022-07-04 21:52:26 +0900

    Works. Not Bootstrapped yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "creating 10 articles..."
+10.times do
+  Article.create(
+    title: Faker::Movie.title,
+    content: Faker::Lorem.paragraph
+  )
+end
+puts "Seeding complete 🌱"
