9e2e2417912351ab04db83fba7878e0af6090bf9
mark_houlli <markhoulli@protonmail.com>
2022-07-05 06:00:39 +0900

    Bootsprap added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -19,17 +20,15 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(article_params)
     redirect_to articles_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     redirect_to articles_path
   end
@@ -40,6 +39,8 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :content)
   end
 
-
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
 end
